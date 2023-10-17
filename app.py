e8b123d1bc63ec470ed0cf4e7564ccaffb952ca5
gas-git <glengardiner@outlook.ie>
2023-10-17 23:50:14 +0100

    update

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import json
 
 import time
 
+import subprocess
+
+
 
 
 app = Flask(__name__)
@@ -16,19 +19,20 @@ def hello():
     current_time = time.strftime("%H:%M:%S", t)
     print(current_time)
 
-    for i in range(1, 5000):
+    for i in range(1, 10):
         with open('stress.json') as stress_file:
             
             data = json.load(stress_file)
             stress_file.close()
 
-            # for item in data['testing']:
-            #     item['Time'] = item['Time'] = current_time
+           
+            for item in data['testing']:
+                item['Time'] = item['Time'] = current_time
 
 
-            # with open('stress.json', "w") as stress_file:
-            #     json.dump(data, stress_file)
-            #     stress_file.close()
+            with open('stress.json', "w") as stress_file:
+                json.dump(data, stress_file)
+                stress_file.close()
 
         
         return {"testing": [{"Name": "Glen Gardiner", "Expertise": "Stressing", "Country_of_birth": "Ireland", "Time": current_time}]}
